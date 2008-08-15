d5db0f4a433b7e2a992d8d9b168b17c8e1e57a4b
Ali Gholami Rudi <aligrudi@users.sf.net>
2008-08-15 12:57:45 +0430

    ironout: add findat command

--- ironout.c
+++ ironout.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include "parse.h"
+#include "ast.h"
 
 
 int main(int argc, char **argv)
@@ -8,6 +9,12 @@ int main(int argc, char **argv)
 	if (argc > 1) {
 		if (!strcmp(argv[1], "parse") && argc > 2) {
 			free_node(parse(argv[2]));
+			return 0;
+		}
+		if (!strcmp(argv[1], "findat") && argc > 3) {
+			struct node *node = parse(argv[2]);
+			node_at(node, atoi(argv[3]));
+			free_node(node);
 			return 0;
 		}
 	}
