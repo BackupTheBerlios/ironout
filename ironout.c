730c32753e8c22343b5b7bf962f8c5e17acdd498
Ali Gholami Rudi <aligrudi@users.sf.net>
2008-08-16 21:49:37 +0430

    ironout: extract commands out of main()

--- ironout.c
+++ ironout.c
@@ -4,19 +4,27 @@
 #include "ast.h"
 
 
+static int parse_cmd(char *filename)
+{
+	free_node(parse(filename));
+	return 0;
+}
+
+static int getname_cmd(char *filename, long offset)
+{
+	struct node *node = parse(filename);
+	node_at(node, offset);
+	free_node(node);
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
 	if (argc > 1) {
-		if (!strcmp(argv[1], "parse") && argc > 2) {
-			free_node(parse(argv[2]));
-			return 0;
-		}
-		if (!strcmp(argv[1], "getname") && argc > 3) {
-			struct node *node = parse(argv[2]);
-			node_at(node, atoi(argv[3]));
-			free_node(node);
-			return 0;
-		}
+		if (!strcmp(argv[1], "parse") && argc > 2)
+			return parse_cmd(argv[2]);
+		if (!strcmp(argv[1], "getname") && argc > 3)
+			return getname_cmd(argv[2], atoi(argv[3]));
 	}
 	printf("Usage: %s COMMAND [ARGS]\n", argv[0]);
 	return 1;
