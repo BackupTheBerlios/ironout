07d34bf5c1fbe7dd3efd1d9535b3e760d883df59
Ali Gholami Rudi <aligrudi@users.sf.net>
2008-08-17 18:59:27 +0430

    test_hash: make test_inthash() more compact

--- tests/test_hash.c
+++ tests/test_hash.c
@@ -34,27 +34,6 @@ static int _intcmp(void *data, void *key)
 	return i1 - i2;
 }
 
-static void test_inthash()
-{
-	struct hash *hash = hash_init(_inthash, _inthash, _intcmp);
-	int a = 0, b = 1;
-	int zero = 0, one = 1;
-
-	/* NULL should be returned if a key is missing */
-	equal(NULL, hash_get(hash, &one), "0");
-
-	/* basic insertion */
-	hash_put(hash, &a);
-	equal(&a, hash_get(hash, &zero), "1");
-
-	/* putting more values */
-	hash_put(hash, &b);
-	equal(&a, hash_get(hash, &zero), "2");
-	equal(&b, hash_get(hash, &one), "3");
-
-	hash_release(hash);
-}
-
 static void test_strhash()
 {
 	struct hash *hash = hash_init(_strhash, _strhash, _strcmp);
@@ -72,6 +51,21 @@ static void test_strhash()
 	hash_put(hash, b);
 	equal(a, hash_get(hash, "a"), "12");
 	equal(b, hash_get(hash, "b"), "13");
+
+	hash_release(hash);
+}
+
+static void test_inthash()
+{
+	struct hash *hash = hash_init(_inthash, _inthash, _intcmp);
+	int a = 0, b = 1, zero = 0, one = 1;
+
+	equal(NULL, hash_get(hash, &one), "0");
+	hash_put(hash, &a);
+	equal(&a, hash_get(hash, &zero), "1");
+	hash_put(hash, &b);
+	equal(&a, hash_get(hash, &zero), "2");
+	equal(&b, hash_get(hash, &one), "3");
 
 	hash_release(hash);
 }
