2a6c745282547d797ee85ad26c518066a8c0db03
Ali Gholami Rudi <aligrudi@users.sf.net>
2008-09-08 04:37:20 +0430

    rename: basic renaming support

---
+++ rename.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include "src.h"
+#include "rename.h"
+#include "src.h"
+
+void rename(struct occurrence *occurrences, char *replacement)
+{
+	struct occurrence *cur = occurrences;
+	while (cur) {
+		struct cfile *cfile = cur->cfile;
+		struct src *orig = src_from_file(cur->cfile->name);
+		struct src *changed = src_copy(orig);
+		long diffs = 0;
+		while (cur && cur->cfile == cfile) {
+			long start = cur->start + diffs;
+			long end = cur->end + diffs;
+			src_delete(changed, start, end);
+			src_insert(changed, start, replacement);
+			cur = cur->next;
+			diffs += strlen(replacement) - (end - start);
+		}
+		src_print_diffs(orig, changed, cfile->name);
+		src_free(orig);
+		src_free(changed);
+	}
+}
---
+++ rename.h
@@ -0,0 +1,10 @@
+#ifndef _RENAME_H
+#define _RENAME_H
+
+#include "cfile.h"
+#include "find.h"
+#include "project.h"
+
+void rename(struct occurrence *occurrences, char *replacement);
+
+#endif
