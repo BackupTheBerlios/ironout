905eb4598f46bfe5c4db1c8b12039112f1f4535e
Ali Gholami Rudi <aligrudi@users.sf.net>
2008-09-16 17:17:34 +0430

    ast: is_field() is no longer used

--- ast.h
+++ ast.h
@@ -112,7 +112,6 @@ void node_free(struct node *node);
 struct node *node_find(struct node *node, long offset);
 void node_walk(struct node *, int (*) (struct node *, void *), void *);
 int node_cmp(struct node *n1, struct node *n2);
-int node_isfield(struct node *node);
 char *declarator_name(struct node *node);
 
 #endif
